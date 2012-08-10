48079663da16ec53b49f845b6912852d3f0bf1fc
Matt Oswald <moswald@gmail.com>
2012-08-10 09:12:05 -0500

    simplified test runner somewhat with module-scoped helpers

--- xUnit++/TestRunner.h
+++ xUnit++/TestRunner.h
@@ -3,30 +3,30 @@
 
 #include <chrono>
 #include <functional>
+#include <memory>
 #include <string>
 #include <vector>
 #include "Fact.h"
 #include "Theory.h"
+#include "xUnitTime.h"
 
 namespace xUnitpp
 {
 
-int RunAllTests(const std::string &suite = "", size_t maxTestRunTime = 0, size_t maxConcurrent = 0);
+size_t RunAllTests(const std::string &suite = "", size_t maxTestRunTime = 0, size_t maxConcurrent = 0);
 
 class TestRunner
 {
 public:
-    TestRunner(/*std::function<void(const TestDetails &)> onTestStart,
+    TestRunner(std::function<void(const TestDetails &)> onTestStart,
                std::function<void(const TestDetails &, const std::string &)> onTestFailure,
-               std::function<void(const TestDetails &, const std::chrono::duration<float, std::milli>)> onTestFinish,
-               std::function<void(int, int, int, const std::chrono::duration<float, std::milli>)> onAllTestsComplete*/);
-    int RunTests(const std::vector<Fact> &facts, const std::vector<Theory> &theories, const std::string &suite, size_t maxTestRunTime = 0, size_t maxConcurrent = 0);
+               std::function<void(const TestDetails &, milliseconds)> onTestFinish,
+               std::function<void(int, int, int, milliseconds)> onAllTestsComplete);
+    size_t RunTests(const std::vector<Fact> &facts, const std::vector<Theory> &theories, const std::string &suite, size_t maxTestRunTime = 0, size_t maxConcurrent = 0);
 
 private:
-    std::function<void(const TestDetails &)> mOnTestStart;
-    std::function<void(const TestDetails &, const std::string &)> mOnTestFailure;
-    std::function<void(const TestDetails &, const std::chrono::duration<float, std::milli>)> mOnTestFinish;
-    std::function<void(int, int, int, const std::chrono::duration<float, std::milli>)> mOnAllTestsComplete;
+    class Impl;
+    std::unique_ptr<Impl> mImpl;
 };
 
 }
