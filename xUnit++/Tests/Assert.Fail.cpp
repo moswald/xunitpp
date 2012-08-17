3a4f91571ba3cc72a3a2bf52909d2798f3b8b292
moswald <moswald@gmail.com>
2012-08-17 14:20:55 -0500

    Assert.False tests added

---
+++ xUnit++/Tests/Assert.Fail.cpp
@@ -0,0 +1,23 @@
+#include "../xUnit++.h"
+
+using xUnitpp::xUnitAssert;
+using xUnitpp::Assert;
+
+SUITE(AssertFail)
+{
+
+FACT(FailAlwaysAsserts)
+{
+    Assert.Throws<xUnitAssert>([]() { Assert.Fail(); });
+}
+
+FACT(FailAppendsMessage)
+{
+    static const std::string msg = "xUnit++";
+
+    auto assert = Assert.Throws<xUnitAssert>([]() { Assert.Fail(msg); });
+
+    Assert.Contains(assert.what(), msg.c_str());
+}
+
+}
