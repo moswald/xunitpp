5fcb570acd08d2929251c57649c226b8626d703a
Matt Oswald <moswald@gmail.com>
2012-08-21 08:39:06 -0500

    Assert.NotInRange tests added

---
+++ xUnit++/Tests/Assert.NotInRange.cpp
@@ -0,0 +1,62 @@
+#include <tuple>
+#include <vector>
+#include "../xUnit++.h"
+
+using xUnitpp::xUnitAssert;
+using xUnitpp::Assert;
+
+SUITE(AssertNotInRange)
+{
+
+std::vector<std::tuple<int, int, int>> NotInRangeSuccessData()
+{
+    std::vector<std::tuple<int, int, int>> data;
+
+    data.push_back(std::make_tuple(-1, 0, 1));
+    data.push_back(std::make_tuple(1, 0, 1));
+    data.push_back(std::make_tuple(2, 0, 1));
+    data.push_back(std::make_tuple(0, -2, -1));
+    data.push_back(std::make_tuple(-1, -2, -1));
+    data.push_back(std::make_tuple(-3, -2, -1));
+
+    return data;
+}
+
+THEORY(NotInRangeSuccess, (int actual, int min, int max), NotInRangeSuccessData)
+{
+    Assert.NotInRange(actual, min, max);
+}
+
+std::vector<std::tuple<int, int, int>> NotInRangeAssertsOnFailData()
+{
+    std::vector<std::tuple<int, int, int>> data;
+
+    data.push_back(std::make_tuple(0, 0, 1));
+    data.push_back(std::make_tuple(2, 1, 3));
+    data.push_back(std::make_tuple(-1, -1, 0));
+    data.push_back(std::make_tuple(-2, -3, -1));
+
+    return data;
+}
+
+THEORY(NotInRangeAssertsOnFailure, (int actual, int min, int max), NotInRangeAssertsOnFailData)
+{
+    Assert.Throws<xUnitAssert>([=]() { Assert.NotInRange(actual, min, max); });
+}
+
+FACT(NotInRangeAppendsUserMessage)
+{
+    static const std::string msg = "xUnit++";
+
+    auto assert = Assert.Throws<xUnitAssert>([=]() { Assert.NotInRange(0, 0, 1, msg); });
+
+    Assert.Contains(assert.what(), msg.c_str());
+}
+
+FACT(NotInRangeNeedsValidRange)
+{
+    Assert.Throws<std::invalid_argument>([]() { Assert.NotInRange(0, 0, 0); });
+    Assert.Throws<std::invalid_argument>([]() { Assert.NotInRange(0, 1, 0); });
+}
+
+}
