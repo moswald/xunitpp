463756e36286a89e7e54e39b90e61d2b48390021
Matt Oswald <moswald@gmail.com>
2012-08-21 17:01:07 -0500

    Assert.NotSame, Assert.Same tests added

---
+++ xUnit++/Tests/Assert.Same.cpp
@@ -0,0 +1,56 @@
+#include "../xUnit++.h"
+
+using xUnitpp::xUnitAssert;
+using xUnitpp::Assert;
+
+SUITE(AssertSame)
+{
+
+FACT(SameSuccess)
+{
+    int x;
+
+    Assert.Same(x, x);
+}
+
+FACT(SameForPointersSuccess)
+{
+    int x;
+
+    Assert.Same(&x, &x);
+}
+
+FACT(SameForConstPointersSuccess)
+{
+    int x;
+    const int *px = &x;
+
+    Assert.Same(px, px);
+}
+
+FACT(SameAssertsOnFailure)
+{
+    int x = 2;
+    int y = 2;
+
+    Assert.Throws<xUnitAssert>([=]() { Assert.Same(x, y); });
+}
+
+FACT(SameForPointersAssertsOnFailure)
+{
+    int x = 2;
+    int y = 2;
+
+    Assert.Throws<xUnitAssert>([=]() { Assert.Same(&x, &y); });
+}
+
+FACT(SameAppendsUserMessage)
+{
+    static const std::string msg = "xUnit++";
+
+    auto assert = Assert.Throws<xUnitAssert>([]() { Assert.Same(0, 1, msg); });
+
+    Assert.Contains(assert.what(), msg.c_str());
+}
+
+}
