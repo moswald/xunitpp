6e3c1a6a9add10d1be9e519365cb8832f05d3fe8
Matt Oswald <moswald@gmail.com>
2012-09-05 15:44:11 -0500

    renamed THEORY to DATA_THEORY, added new THEORY macro that has inline data

--- xUnit++/xUnit++/xUnitMacroHelpers.h
+++ xUnit++/xUnit++/xUnitMacroHelpers.h
@@ -41,6 +41,8 @@
 
 #define _xPP_NARGS_IMPL(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,N,...) N
 
+#define FIRST_ARG(x, ...) x
+
 // attribute expansion helpers
 #define XU_A_1(a) \
     attributes.emplace(std::make_pair a);
