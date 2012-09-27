7638c5bc7526e15885cdcaf21695e780991fea88
moswald <moswald@gmail.com>
2012-09-26 23:14:07 -0500

    removed some debugging code

--- Tests/UnitTests/Assert.DoesNotContain.cpp
+++ Tests/UnitTests/Assert.DoesNotContain.cpp
@@ -21,36 +21,6 @@ FACT("DoesNotContainForSequenceAssertsOnFailure")
     Assert.Throws<xUnitAssert>([&]() { Assert.DoesNotContain(v, 0); });
 }
 
-namespace TestNs_24
-{
-    using xUnitpp::Assert;
-    std::shared_ptr<xUnitpp::Check> pCheck = std::make_shared<xUnitpp::Check>(); 
-    class TestFixture_24 : public xUnitpp::NoFixture 
-    {
-        TestFixture_24 &operator =(TestFixture_24);
-    public:
-        TestFixture_24() : Check(*pCheck) { }
-        void TestFn_24();
-        
-        const xUnitpp::Check &Check;
-    };
-    void TestRunner_24()
-    {
-        TestFixture_24().TestFn_24();
-    }
-    xUnitpp::TestCollection::Register reg(xUnitpp::TestCollection::Instance(), &TestRunner_24, "ContainsForSequenceAppendsUserMessage", xUnitSuite::Name(), xUnitAttributes::Attributes(), -1, "assert.contains.cpp", 24, pCheck);
-}
-void TestNs_24 :: TestFixture_24 :: TestFn_24()
-{
-    static const std::string msg = "xUnit++";
-    std::vector<int> v;
-
-    auto assert = Assert.Throws<xUnitAssert>([&]() { Assert.Contains(v, 0) << msg; });
-    auto what = std::string(assert.what());
-
-    Assert.NotEqual(std::string::npos, what.find(msg));
-}
-
 FACT("DoesNotContainForSequenceAppendsUserMessage")
 {
     static const std::string msg = "xUnit++";
